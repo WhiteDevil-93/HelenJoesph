c51d038e390f002b6cb1d2e7fed089712f91d452
Tashriq Hendricks <tashriq93@gmail.com>
2026-07-02 19:26:55 +0200

    v4.1: service worker kill switch

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,11 @@
-// Titrate Service Worker v4.1 - Cache buster
-const CACHE_NAME = 'titrate-v4.1';
-const FILES = ['./','./index.html','./app.js','./data.json'];
-
+// Kill switch - v4.1
 self.addEventListener('install', e => {
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+  e.waitUntil(caches.keys().then(names => Promise.all(names.map(n => caches.delete(n)))));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(names => Promise.all(
-      names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
-    )).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(names => Promise.all(names.map(n => caches.delete(n))))).then(() => self.clients.claim());
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request, { cache: 'no-store' }).then(r => {
-      const clone = r.clone();
-      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
-      return r;
-    }).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request, { cache: 'no-store' }));
 });
